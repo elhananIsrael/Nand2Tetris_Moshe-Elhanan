a3c5a02d38cfcd8b448d4281c1e2377d5e5b97f6
Heller <eellhhaannaann@gmail.com>
2019-06-30 19:32:01 +0300

    copy files from exe_4 to exe_5 need to update this copy files in exe_5

---
+++ Exe_5/src/Main.kt
@@ -0,0 +1,44 @@
+import java.io.IOException
+import java.io.File
+import java.nio.file.Path
+
+
+fun main() {
+
+    try
+    {
+        println("Please enter input dir path:")
+        var inputDirPath = readLine()!!
+
+        println("Please enter output dir path:")
+        var outputDirPath = readLine()
+
+        File(inputDirPath).walkTopDown().forEach {
+            if (it.isFile)
+                if (File(it.name).extension == "jack") {
+
+                    var outputFileTokenizer = outputDirPath + '\\' + File(it.name).nameWithoutExtension + "T.xml"
+                    if (File(outputFileTokenizer).exists()) {
+                        File(outputFileTokenizer).delete()
+                    }
+
+                    var tokenizer: JackTokenizer = JackTokenizer(it.path)
+                    tokenizer.writeTokInOutputFile(outputFileTokenizer)
+
+                    var outputFileParser = outputDirPath + '\\' + File(it.name).nameWithoutExtension + ".xml"
+                    if (File(outputFileParser).exists()) {
+                        File(outputFileParser).delete()
+                    }
+
+                    var parser: CompilationEngine = CompilationEngine(outputFileTokenizer, outputFileParser)
+
+                }
+        }
+    }
+catch (e:IOException) {
+    println("IOException: error.")
+}
+}
+
+//C:\MyProjects\intellijProjects\examples\tests\exe4Test         //input test
+//C:\MyProjects\intellijProjects\examples\tests\exe4Output       //output
